a667f35216a7cf65cb3b685cdb44de4cde6f5d74
DaffiX <suleydaffy@gmail.com>
2022-09-13 09:47:52 -0700

    Program for putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Papa";
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is nexted quiz for c programming 
