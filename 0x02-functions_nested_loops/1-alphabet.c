098333add3d4287093c66d8c58a049383428e39a
DaffiX <suleydaffy@gmail.com>
2022-09-13 16:10:09 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alx;
+
+	for (alx = 'a'; alpha <= 'z'; alx++)
+	{
+		_putchar(alx);
+	}
+	_putchar('\n');
+}
