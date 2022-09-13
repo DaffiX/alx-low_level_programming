effef46547456d30428e386c107df04f30e69e80
DaffiX <suleydaffy@gmail.com>
2022-09-13 16:57:55 -0700

    The error ms

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	_putchar('A');
-	_putchar('l');
-	_putchar('x');
-	_putchar('s');
-	_putchar('e');
-	_putchar('p');
-	_putchar('r');
+	_putchar('H');
 	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
