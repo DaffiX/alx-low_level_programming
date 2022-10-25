1aa00ccdabada87e5962c0471d03b2a8183b08e5
DaffiX <suleydaffy@gmail.com>
2022-10-25 14:09:40 -0700

    Slt updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,29 +1,38 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-  * add_nodeint_end - add new nodes to the end of the list
-  * @head: current place in the list
-  * @n: int to add to the list
-  * Return: pointer to current position in list
+  * insert_nodeint_at_index - insert a node at index
+  * @head: head of the list
+  * @index: location to insert node
+  * @n: value of the inserted node
+  * Return: pointer to head of list
   */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	listint_t *new, *current;
+	listint_t *current, *new;
 
+	current = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
+	if (index == 0)
 	{
+		new->next = current;
 		*head = new;
 		return (*head);
 	}
-	current = *head;
-	while (current->next != NULL)
+	while (index > 1)
+	{
 		current = current->next;
+		index--;
+		if (!current)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
 	current->next = new;
-	return (*head);
+	return (new);
 }
